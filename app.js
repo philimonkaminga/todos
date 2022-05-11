f4868aa5a50c7b60b7ecd4a7eb2f0080ad49caae
Philimon Kaminga <philimonkaminga@gmail.com>
2022-05-11 21:45:02 +0200

    ability to create new todos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const todoInput = document.getElementById('add-todo');
+const addTodoBtn = document.getElementById('add-todo-btn');
+
+addTodoBtn.addEventListener('click', (e)=>{
+    e.preventDefault();  
+    
+    const liElement = document.createElement('li');
+    const todoList = document.getElementById('todo-list');
+
+    liElement.textContent = todoInput.value; 
+
+    todoList.appendChild(liElement);
+    todoInput.value = '';
+});
